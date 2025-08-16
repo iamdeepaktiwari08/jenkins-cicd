93e7a53e1bb4c6443532b733ae0dcc49838d527d
Super User <root@localhost.localdomain>
2025-08-16 07:43:45 +0530

    fourth

--- test_apache_page.py
+++ test_apache_page.py
@@ -1,25 +1,24 @@
 import sys
-import os
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
 
 if len(sys.argv) < 2:
-    print("❌ Usage: python3 test_selenium.py <URL>")
+    print("❌ Usage: python3 test_apache_page.py <URL>")
     sys.exit(1)
 
 url = sys.argv[1]
 
-# 🔹 Path to geckodriver (absolute path)
-# Windows: r"C:\Users\deepak\Downloads\geckodriver.exe"
-# Linux: "/usr/local/bin/geckodriver"
-GECKO_PATH = os.path.abspath("geckodriver")
+# Headless mode for Jenkins
+options = Options()
+options.add_argument("--headless")
 
-service = FirefoxService(executable_path=GECKO_PATH)
-driver = webdriver.Firefox(service=service)
+service = Service("/usr/local/bin/geckodriver")
+driver = webdriver.Firefox(service=service, options=options)
 
 driver.get(url)
 
 assert "Apache" in driver.title, "❌ Apache page title mismatch!"
 
-print("✅ Apache page loaded successfully on Firefox!")
+print("✅ Apache page loaded successfully on Firefox (Jenkins headless)!")
 driver.quit()
